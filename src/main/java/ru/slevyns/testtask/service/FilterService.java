2ceaeccd808c3a77b14374b66396f84a76f46e83
Alexey.Gavenko <alexey.gavenko@nordclan.com>
2024-04-28 15:46:57 +0300

    feature: filter interface and impl

---
+++ src/main/java/ru/slevyns/testtask/service/FilterService.java
@@ -0,0 +1,7 @@
+package ru.slevyns.testtask.service;
+
+import ru.slevyns.testtask.domain.WordCollection;
+
+public interface FilterService {
+    void filter(WordCollection collection);
+}
---
+++ src/main/java/ru/slevyns/testtask/service/impl/BiggerWordsFilterServiceImpl.java
@@ -0,0 +1,11 @@
+package ru.slevyns.testtask.service.impl;
+
+import ru.slevyns.testtask.domain.WordCollection;
+import ru.slevyns.testtask.service.FilterService;
+
+public class BiggerWordsFilterServiceImpl implements FilterService {
+    @Override
+    public void filter(WordCollection collection) {
+
+    }
+}
